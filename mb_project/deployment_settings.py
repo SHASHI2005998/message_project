2cb631a081f4ff6b07a87c442f67274d4e6c7724
Shashikumar T <sk3332441@gmail.com>
2026-02-11 10:48:19 +0530

    Intial commit

---
+++ mb_project/deployment_settings.py
@@ -0,0 +1,8 @@
+import os
+import dj_database_url
+from .settings import *
+from .settings import BASE_DIR
+ALLOWED_HOSTS=[os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+CSRF_TRUSTED_ORIFINS=[os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+DEBUG=False
+SECRET_KEY=os.environ.get['SECRET_KEY']
--- mb_project/wsgi.py
+++ mb_project/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mb_project.settings')
+settings_module = 'mb_project.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'mb_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module)
 
 application = get_wsgi_application()
