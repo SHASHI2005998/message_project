68b8cb53570d23053342b8eed807c6767c7974f0
SHASHI2005998 <sk3332441@gmail.com>
2026-04-07 20:39:23 +0530

    Update settings.py

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -91,9 +91,8 @@ TEMPLATES = [
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True   # ✅ IMPORTANT CHANGE
     )
 }
 
